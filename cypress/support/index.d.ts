a9b9ad842494d73f0d405130a5f04f9666da02da
Rhapsodya Piedro Asmorobangun <piedrorhapsodya@gmail.com>
2025-09-17 15:38:43 +0700

    refactor: remove eslint disable lines

--- cypress/support/index.d.ts
+++ cypress/support/index.d.ts
@@ -1,12 +1,5 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-
 declare namespace Cypress {
   interface Chainable {
-    /**
-     * Custom command to mount a Vue component in a Cypress test.
-     * @example cy.mount(MyComponent)
-     */
-    mount(component: any, options?: any): Chainable;
     /**
      * Custom command to select DOM element by data-wv-section attribute.
      * @example cy.getSection('title')
